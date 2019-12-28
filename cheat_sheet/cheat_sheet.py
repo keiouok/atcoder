8e252af0365b04978cff1c21e357b75920489b29
keiouok <ko@ss.cs.osakafu-u.ac.jp>
2019-12-28 20:27:26 +0900

    add gcd, lcm to cheat sheet

--- cheat_sheet/cheat_sheet.py
+++ cheat_sheet/cheat_sheet.py
@@ -6,9 +6,28 @@ from operator import itemgetter, mul
 from copy import deepcopy
 from string import ascii_lowercase, ascii_uppercase, digits
 from fractions import gcd
-
+ 
 def input(): return sys.stdin.readline().strip()
 def INT(): return int(input())
 def MAP(): return map(int, input().split())
+def S_MAP(): return map(str, input().split())
 def LIST(): return list(map(int, input().split()))
-def S_LIST(): return list(input())
+def S_LIST(): return list(map(str, input().split()))
+ 
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+# 最小公倍数
+def gcd(a, b):
+  if a < b:
+      a, b = b, a
+  while a % b != 0:
+      a, b = b, a % b
+  return b
+ 
+# 最大公約数 
+def lcm(a, b):
+    y = a*b / gcd(a, b)
+    return int(y)
+ 
