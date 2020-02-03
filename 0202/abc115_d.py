a74a2d86158e4c69a74410d47d74e35780649f01
keiouok <ko@ss.cs.osakafu-u.ac.jp>
2020-02-03 19:48:56 +0900

    christmas done!

--- 0202/abc115_d.py
+++ 0202/abc115_d.py
@@ -29,10 +29,13 @@ for i in range(N):
 
 def f(N, X):
     if N == 0:
-        return 0 if X <= 0 else 1
-    elif X <= 1 + a[N-1]:
+        if X <= 0:
+            return 0
+        if X > 0:
+            return 1
+    elif X <= a[N-1] + 1:
         return f(N-1, X-1)
-    else:
-        return p[N-1] + 1 + f(N-1, X-2-a[N-1])
+    elif X > a[N-1] + 1:
+        return p[N-1] + 1 + f(N-1, X - (a[N-1] + 1 + 1))
 
 print(f(N, X))
