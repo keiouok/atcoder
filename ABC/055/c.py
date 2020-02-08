443d0d0971794a743878259eb2c1b2476b1133f3
keiouok <ko@ss.cs.osakafu-u.ac.jp>
2020-02-08 22:53:56 +0900

    0208_2253

---
+++ ABC/055/c.py
@@ -0,0 +1,23 @@
+import sys, re, os
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians
+from itertools import permutations, combinations, product, accumulate
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from fractions import gcd
+ 
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def S_MAP(): return map(str, input().split())
+def LIST(): return list(map(int, input().split()))
+def S_LIST(): return list(map(str, input().split()))
+ 
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+n, m = MAP()
+
+print(n + (m - 2 * n) // 4 if m >= 2 * n else m // 2)
