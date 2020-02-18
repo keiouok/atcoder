21f75d100efb6f60288bcdc086ef8e081d9704f4
keiouok <ko@ss.cs.osakafu-u.ac.jp>
2020-02-18 17:07:33 +0900

    auto door

---
+++ ABC/024/b.py
@@ -0,0 +1,33 @@
+import sys, re, os
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians
+from itertools import permutations, combinations, product, accumulate
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from fractions import gcd
+ 
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def S_MAP(): return map(str, input().split())
+def LIST(): return list(map(int, input().split()))
+def S_LIST(): return list(map(str, input().split()))
+ 
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+N, T = MAP()
+A = [INT() for i in range(N)]
+ans = 0
+
+for i in range(N-1):
+    if A[i+1] - A[i] > T:
+        ans += T
+    else:
+        ans += A[i+1] - A[i]
+ans += T
+print(ans)
+
+    
