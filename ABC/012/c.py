5b87ae36e370613d213e02f766ca59c8fda2b52c
keiouok <ko@ss.cs.osakafu-u.ac.jp>
2020-03-01 00:35:22 +0900

    0301

---
+++ ABC/012/c.py
@@ -0,0 +1,37 @@
+import sys, re, os
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians
+from itertools import permutations, combinations, product, accumulate
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from fractions import gcd
+ 
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def S_MAP(): return map(str, input().split())
+def LIST(): return list(map(int, input().split()))
+def S_LIST(): return list(map(str, input().split()))
+ 
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+
+N = INT()
+wa = 0
+for i in range(1, 10):
+    for j in range(1, 10):
+        wa += i * j
+
+left = wa - N
+
+ans = []
+for i in range(1, 10):
+    for j in range(1, 10):
+        if i * j == left:
+            ans.append(str(i) + " x " + str(j))
+ans.sort()
+print(*ans, sep="\n")
+
