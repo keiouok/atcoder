3bcdac74da38387191e4ae46f261021f21167f6d
keiouok <ko@ss.cs.osakafu-u.ac.jp>
2020-03-09 00:56:55 +0900

    compensated_b_problems

---
+++ 0308/abc012_b.py
@@ -0,0 +1,29 @@
+import sys, re, os
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians
+from itertools import permutations, combinations, product, accumulate
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from fractions import gcd
+ 
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def S_MAP(): return map(str, input().split())
+def LIST(): return list(map(int, input().split()))
+def S_LIST(): return list(map(str, input().split()))
+ 
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+N = INT()
+h = N // 3600
+m = (N % 3600) // 60
+s = ((N % 3600) % 60)
+
+h = str(h).zfill(2)
+m = str(m).zfill(2)
+s = str(s).zfill(2)
+print(h+":"+m+":"+s)
