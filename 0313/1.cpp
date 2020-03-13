941dff6bd32f260643d8121ac3f8f6cd45bfd25d
root <root@LAPTOP-TJ0D7L1P.localdomain>
2020-03-13 23:17:53 +0900

    0313

---
+++ 0313/1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << 1 + 1 << endl; // 2
+  cout << 3 - 4 << endl; // -1
+  cout << 2 * 3 << endl; // 6
+  cout << 7 / 3 << endl; // 2
+}
