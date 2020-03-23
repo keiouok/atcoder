7a4ef046279aad61aeed243eb13493551e3a9fde
keiouok <ko@ss.cs.osakafu-u.ac.jp>
2020-03-23 23:17:31 +0900

    0323_for_preparing_guraduation

---
+++ 0323/a.py
@@ -0,0 +1,32 @@
+import sys, re, os
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians
+from itertools import permutations, combinations, product, accumulate
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from fractions import gcd
+ 
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def S_MAP(): return map(str, input().split())
+def LIST(): return list(map(int, input().split()))
+def S_LIST(): return list(map(str, input().split()))
+ 
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+A, K = MAP()
+t = A
+
+if K == 0:
+    print(2 * 10 ** 12 - A)
+else:
+    day = 0
+    while t < 2 * 10 ** 12:
+        t = t + (1 + K * t)
+        day += 1
+    print(day)
+    
