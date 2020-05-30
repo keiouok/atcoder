2ba1147ee59090b3fb23906254f5251c3ca75b1c
keiouok <ko.yuka.kp2@is.naist.jp>
2020-05-30 21:36:48 +0900

    +arc_nomura

---
+++ ARC/NOMURA/a.py
@@ -0,0 +1,22 @@
+import sys, re
+from collections import deque, defaultdict, Counter
+from math import ceil, sqrt, hypot, factorial, pi, sin, cos, radians
+from itertools import accumulate, permutations, combinations, product
+from operator import itemgetter, mul
+from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase, digits
+from bisect import bisect, bisect_left
+from fractions import gcd
+from heapq import heappush, heappop
+from functools import reduce
+def input(): return sys.stdin.readline().strip()
+def INT(): return int(input())
+def MAP(): return map(int, input().split())
+def LIST(): return list(map(int, input().split()))
+def ZIP(n): return zip(*(MAP() for _ in range(n)))
+sys.setrecursionlimit(10 ** 9)
+INF = float('inf')
+mod = 10 ** 9 + 7
+
+h1, m1, h2, m2, k = MAP()
+print(h2 * 60 + m2 - h1 * 60 - m1 - k)
