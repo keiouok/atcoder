a4c2d11928eb024eb81a0e91747a8484e8a71ec7
keiouok <ko.yuka.kp2@is.naist.jp>
2021-01-10 23:56:19 +0900

    [ADD] 0110 d.cpp ongoing

--- 2021/1/10/d.cpp
+++ 2021/1/10/d.cpp
@@ -2,4 +2,18 @@
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-#define rep(i, n) for 
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main(){
+    int n, C;
+    cin >> n >> C;
+    map<int, ll> events;
+    rep(i, n){
+        int a, b, c;
+        cin >> a >> b >> c;
+        events[a] += c;
+        events[b+1] -= c;
+    }
+}
