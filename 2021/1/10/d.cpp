8b6d48906acf67c99304a9586bdb0ece95b500e5
keiouok <ko.yuka.kp2@is.naist.jp>
2021-01-11 01:18:36 +0900

    [ADD] 0110 d_ans.py zaatsu a little rikai

--- 2021/1/10/d.cpp
+++ 2021/1/10/d.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
+// #include <atcoder/all>
+// using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 using ll = long long;
 using P = pair<int, int>;
 
-int main(){
+int main() {
     int n, C;
     cin >> n >> C;
     map<int, ll> events;
-    rep(i, n){
+    rep(i, n) {
         int a, b, c;
         cin >> a >> b >> c;
         events[a] += c;
         events[b+1] -= c;
+    }
+    ll s = 0;
+    for (auto event : events) {
+        s += event.second;
     }
 }
--- 2021/1/10/d_ans.py
+++ 2021/1/10/d_ans.py
@@ -20,20 +20,38 @@ mod = 10 ** 9 + 7
 N, C = MAP()
 L = [LIST() for i in range(N)]
 
-event = []
-for i in range(N):
-    a, b, c = L[i]
-    a -= 1
-    event.append((a, c))
-    event.append((b, -c))
-    
-event.sort()
+info_set = set()
+
+for a, b, c in L:
+    info_set.add(a)
+    info_set.add(b+1)
+
+dates = sorted(info_set)
+# print(dates)
+# 本当に日付：圧縮する対象の座標
+dates_d = {date:idx for idx, date in enumerate(dates)}
+# print(dates_d)
+M = len(dates)
+
+imos = [0] * (M + 1)
+
+for a, b, c in L:
+    # a, b : datesのkey
+    a_idx = dates_d[a]
+    b_idx = dates_d[b+1]
+    imos[a_idx] += c
+    imos[b_idx] -= c
+
+cum_sum = [0] * (M + 1)
+
+for i in range(M):
+    cum_sum[i+1] = cum_sum[i] + imos[i]
+    # imos[i+1] = imos[i+1] + imos[i]
+
 ans = 0
-fee = 0
-t = 0
-for x, y in event:
-    if x != t:
-        ans += min(C, fee) * (x - t)
-        t = x
-    fee += y
+
+for i in range(M - 1):
+    # add = min(imos[i+1], C) * (dates[i+1] - dates[i])
+    add = min(cum_sum[i+1], C) * (dates[i+1] - dates[i])
+    ans += add
 print(ans)
